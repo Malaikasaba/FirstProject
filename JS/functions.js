2785e2f4f025f8c94f9095c2cd37898f678f69dc
Malaika Saba <malaikasaba3@gmail.com>
2023-11-01 11:55:01 +0500

    firstProject

---
+++ JS/functions.js
@@ -0,0 +1,69 @@
+// const myself = 
+// {
+//     name:{
+//         f_name:"Malaika",
+//         l_name:"Saba",
+//     },
+//     age:20,
+//     gender:"Female",
+//     degree:{
+//     department:"Computer Science",
+//     program:"Software Engineering"
+//     },}
+// console.log("My name:" + myself.name.f_name +myself.name.l_name + "MY age " + myself.age +
+// "I am a:" + myself.gender + "I am a student of" + myself.degree.department + myself.degree.program)
+
+// Task 2 Objects
+//  Addition
+// var a= 10;
+// var b= 15;
+// var additionResult = a+b;
+// console.log("Addition of a and b is:" +additionResult);
+
+// //  Subtraction
+// var c= 20;
+// var d= 15;
+// var subtractionResult = c - d;
+// console.log("Subtraction of c and d is:" +subtractionResult);
+
+// //  Multiplication
+// var num1= 10;
+// var num2= 15;
+// var mResult = num1*num2;
+// console.log("Multiplication of num1 and num2 is:" + mResult);
+
+// //  Division
+// var num3= 10;
+// var num4= 15;
+// var dResult = num3/num4;
+// console.log("Division of num3 and num4 is:" +dResult);
+
+// Arrow Function
+ var add=()=>{
+    var a = 10;
+    var b= 10;
+    var addResult = a+b;
+    console.log("Addition  of a and b is:"+ addResult);
+ }
+ var multipy=()=>{
+    var c= 20;
+    var d= 20;
+    var mResult = c*d;
+    console.log("Multiplication  of c and d is:"+ mResult);
+ }
+ var subtract=()=>{
+    var num1= 30;
+    var num2= 30;
+    var sResult = num1-num2;
+    console.log("Subtraction of num1 and num2 is:" + sResult);
+ }
+ var divisn=()=>{
+    var num3= 49;
+    var num4= 7;
+    var dResult = num3 / num4;
+    console.log("Division od num3 and num4 is:"+ dResult);
+ }
+ add();
+ multipy();
+ subtract();
+ divisn();
---
+++ JS/index.js
@@ -0,0 +1,78 @@
+
+// var  a=52;
+
+// if(a>=90){
+//     console.log("grade A+")
+// }
+// else if(a>=80){
+//     console.log("grade A")
+// }
+// else if(a>=70){
+//     console.log("grade B")
+// }
+// else if(a>= 60){
+//     console.log("grade C")
+// }
+// else if(a>=50){
+//     console.log("grade D")
+// }
+// else{
+//     console.log("grade F")
+// }
+// Task 2
+// const a = 2;
+// for(let i=0; i<=10; i++){
+//     const product = a*i;
+//     console.log( a + "x" + i + "=" + product);
+// }
+
+// Task 3 Even numbers
+// const num=[1,2,3,4,5,6,7,8,9,10,];
+//     for(let i=1; i<=num.length; i++){
+
+//     if(num[i]%2==0){
+//         console.log(num[i] + "is an even number")
+//     }
+//     }
+// ODD Number
+// const num=[1,2,3,4,5,6,7,8,9,10,];
+//     for(let i=1; i<=num.length; i++){
+
+//     if(num[i]%2!=0){
+//         console.log(num[i] + "is an odd number")
+//     }}
+    // SALARY TASK
+    var m_salary = 70000;
+    var ysalary = 12*50000;
+    if(m_salary<50000){
+
+        console.log("Your monthly salary is:" + msalary)
+    }
+    else if(m_salary>=50000 && m_salary <=100000){
+        m_tax = m_salary*(2.5/100);
+        var m_salary = m_salary - m_tax;
+        const y_salary = m_salry*12;
+        console.log("your yearly salary afetr text is:")
+    }
+    else{
+        console.log("You dont have to pay tax")
+    }
+//   task 2nd
+//     const shopping = 7000;
+//   if(shopping<=5000){
+//     console.log("No discount");
+//   }
+//   else if(shopping>5000 && shopping<=10000){
+//     const discount = shopping *.05;
+//     const discountedAmount = shopping - discount;
+//     console.log("5% percent discount. Your amount is" + {discountedAmount});
+//   }
+//   else if(shopping>10000 && shopping<=15000){
+//     const discount = shopping *.1;
+//     const discountedAmount = shopping - discount;
+//     console.log("10% percent discount. Your amount is" + {discountedAmount});}
+//   else{
+//     const discount = shopping *.15;
+//     const discountedAmount = shopping - discount;
+//     console.log("15% percent discount. Your amount is" + {discountedAmount});}
+    
